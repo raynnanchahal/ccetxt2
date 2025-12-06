9acab89d495c261ebdc32bbc1bf3551f31f4b7e9
raynnanchahal <raynnanchahal@gmail.com>
2025-12-06 14:41:52 +0545

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,14 @@
+// Restore real route after GitHub Pages redirect
+(() => {
+  const url = new URL(window.location.href);
+  const p = url.searchParams.get("p");
+  if (p) {
+    url.searchParams.delete("p");
+    window.history.replaceState({}, "", p);
+  }
+})();
+
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
